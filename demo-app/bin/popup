db864108daa13f8291e7999c1adfa642c3de4d33
threaldanish <hussainidanish347@yahoo.com>
2022-02-11 16:50:25 +0530

    First Commit

---
+++ demo-app/bin/popup
@@ -0,0 +1,17 @@
+#!/usr/bin/python3
+
+import tkinter as tk
+from tkinter import messagebox
+import sys
+
+#argcc = len(sys.argv)
+
+#print("The number of arguments is: ", argcc-1)
+
+#CardUID = str(sys.argv[1])
+
+rot = tk.Tk()
+rot.withdraw()
+
+messagebox.showinfo("Card UID","12345")
+
---
+++ demo-app/setup.py
@@ -0,0 +1,26 @@
+#!/usr/bin/python3
+
+import os
+import setuptools
+
+this = os.path.dirname(os.path.realpath(__file__))
+def read(name):
+    with open(os.path.join(this, name)) as f:
+        return f.read()
+
+setuptools.setup(
+    name="popup",
+    version="1.0",
+    author="Syed Danish Hussaini",
+    author_email="hussainidanish347@yahoo.com",
+    description="A small demo project for QT5 apps",
+    classifiers=[
+        "Programming Language :: Python :: 3.6",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+    ],
+    packages=['myapp_source'],
+    include_package_data=True,
+    install_requires=read('requirements.txt'),
+    scripts=['bin/popup']
+)
